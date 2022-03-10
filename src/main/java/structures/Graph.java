689b9956c8cce44e764684f575662a7f486f8433
laitacende <aleksandra.golebiowska600@gmail.com>
2022-03-10 16:02:46 +0100

    Add maven plugins

--- src/main/java/structures/Graph.java
+++ src/main/java/structures/Graph.java
@@ -1,10 +1,13 @@
 package structures;
 
+import java.util.Arrays;
+
 public class Graph {
     public Double[][] adjacencyMatrix;
 
     public Graph(Integer vNo) {
-        adjacencyMatrix = new Double[vNo][vNo];
+        // vNo + 1 because nodes are enumerated from 1 to vNo
+        adjacencyMatrix = new Double[vNo + 1][vNo + 1];
     }
 
     public void addEdge(Integer source, Integer destination, Double distance) {
@@ -13,5 +16,15 @@ public class Graph {
 
     public Double getEdge(Integer source, Integer destination) {
         return adjacencyMatrix[source][destination];
+    }
+
+    public void printAdjacencyMatrix() {
+      //  System.out.print(Arrays.deepToString(adjacencyMatrix));
+        for (int i = 1; i < adjacencyMatrix.length; i++) {
+            for (int j = 1; j < adjacencyMatrix.length; j++) {
+                System.out.print(String.format("%6s", adjacencyMatrix[i][j].toString()));
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/utils/GraphCreator.java
+++ src/main/java/utils/GraphCreator.java
@@ -4,16 +4,46 @@ import org.apache.commons.math3.random.MersenneTwister;
 import structures.Graph;
 
 public class GraphCreator {
+
+    // to obtain directed graph with random distances
     public static Graph randomFullMatrix(Integer vNo, Integer bound) {
         MersenneTwister mt = new MersenneTwister();
         Graph g = new Graph(vNo);
-        for (int i = 0; i < vNo; i++) {
-            for (int j = 0; j < vNo; j++) {
+        for (int i = 1; i < vNo + 1; i++) {
+            for (int j = 1; j < vNo + 1; j++) {
                 if (i == j) {
                     g.addEdge(i, j, -1.0);
+                } else {
+                    if (bound == 0) { // bound not specified, get full range
+                        g.addEdge(i, j, (double) mt.nextInt());
+                    } else {
+                        g.addEdge(i, j, (double) mt.nextInt(bound));
+                    }
                 }
-//                g.addEdge(i, j, M);
             }
         }
+        return g;
+    }
+
+    // to obtain undirected graph with random distances
+    public static Graph randomSymmetric(Integer vNo, Integer bound) {
+        MersenneTwister mt = new MersenneTwister();
+        Graph g = new Graph(vNo);
+        for (int i = 1; i < vNo + 1; i++) {
+            for (int j = 1; j < vNo + 1; j++) {
+                if (i == j) {
+                    g.addEdge(i, j, -1.0);
+                } else if (i > j) {
+                    if (bound == 0) {
+                        g.addEdge(i, j, (double) mt.nextInt(bound));
+                        g.addEdge(j, i, g.getEdge(i, j));
+                    } else {
+                        g.addEdge(i, j, (double) mt.nextInt());
+                        g.addEdge(j, i, g.getEdge(i, j));
+                    }
+                }
+            }
+        }
+        return g;
     }
 }
