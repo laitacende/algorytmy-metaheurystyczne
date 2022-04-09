cddd980913aa174f267a9d09e3406e974cd45278
laitacende <aleksandra.golebiowska600@gmail.com>
2022-04-09 19:32:52 +0200

    Clean th code

--- src/main/java/utils/InvertNeighbourhood.java
+++ src/main/java/utils/InvertNeighbourhood.java
@@ -7,12 +7,15 @@ import java.util.List;
 
 public class InvertNeighbourhood implements INeighbourhood {
 
+    // indices - move to be added to tabu list
     @Override
     public List<Integer> getBestNeighbour(List<Integer> permutation, Graph g, Integer[] indices) {
         List<Integer> newPermutation;
         List<Integer> currentPermutation = new ArrayList<>(permutation);
         Double currentDistance = CostFunction.calcCostFunction(currentPermutation, g);
         Double newDistance;
+        Double newDistanceBest = currentDistance;
+        List<Integer> newPermutationBest = new ArrayList<>();
         int size = currentPermutation.size();
 
         // invert
@@ -20,34 +23,31 @@ public class InvertNeighbourhood implements INeighbourhood {
             for (int j = i + 1; j < g.vNo - 1; j++) { // check from position 'onwards'
                 // change i and j and reverse everything between them
                 newPermutation = new ArrayList<>(currentPermutation);
-                newDistance = currentDistance -
-                        (i != 0 ? g.getEdge(i - 1, i)
-                                : g.getEdge(size -  1, i))
-                        - (j != size - 1 ? g.getEdge(j, j + 1)
-                                : g.getEdge(j, 0))
-                        + (i == 0 && j == size - 1 ? g.getEdge(j, 0) : 0); // subtract connecting edges
+                newDistance = currentDistance;
+
                 // reverse
                 for (int k = 0; k <= j - i; k++) {
                     newPermutation.set(i + k, currentPermutation.get(j - k));
                     // modify distance
-                    newDistance -= (j - k != 0 ? g.getEdge(j - k - 1, j - k)
-                                    : 0);
-                    newDistance += (i + k != 0 ? g.getEdge(i + k - 1, i + k) : 0);
+                    newDistance -= g.getEdge(currentPermutation.get(Math.floorMod(j - k - 1, size)), currentPermutation.get(j - k));
+                    newDistance += j - k != i ? g.getEdge(currentPermutation.get(j - k), currentPermutation.get(Math.floorMod(j - k - 1, size))) : 0;
                 }
-                // add 'cycle' edge
-                newDistance += g.getEdge(size - 1, 0);
+                if (Math.floorMod(j + 1, size) != i) {
+                    newDistance -= g.getEdge(currentPermutation.get(j), currentPermutation.get(Math.floorMod(j + 1, size)));
+                    newDistance += g.getEdge(newPermutation.get(j), newPermutation.get(Math.floorMod(j + 1, size)));
+                    newDistance += g.getEdge(newPermutation.get(Math.floorMod(i - 1, size)), newPermutation.get(i));
+                } else {
+                    newDistance += g.getEdge(newPermutation.get(Math.floorMod(i - 1, size)), newPermutation.get(i));
+                }
 
-                // calculate distance
-               // newDistance = CostFunction.calcCostFunction(newPermutation, g);
-                // TODO check if this calculation is ok
-
-
-                if (newDistance < currentDistance) {
-                    currentPermutation = new ArrayList<>(newPermutation);
-                    currentDistance = newDistance;
+                if (newDistance < newDistanceBest) {
+                    newPermutationBest = new ArrayList<>(newPermutation);
+                    newDistanceBest = newDistance;
+                    indices[0] = i;
+                    indices[1] = j;
                 }
             }
         }
-        return currentPermutation;
+        return newPermutationBest;
     }
 }
--- src/main/java/utils/SwapNeighbourhood.java
+++ src/main/java/utils/SwapNeighbourhood.java
@@ -19,42 +19,27 @@ public class SwapNeighbourhood implements INeighbourhood {
             for (int j = i + 1; j < g.vNo - 1; j++) { // check from position 'onwards'
                 newPermutation = new ArrayList<>(currentPermutation);
                 newDistance = currentDistance;
-                //System.out.println(currentDistance);
                 // swap i and j
                 newPermutation.set(j, currentPermutation.get(i));
                 newDistance -= g.getEdge(currentPermutation.get(Math.floorMod(i - 1, size)), currentPermutation.get(i));
-               // System.out.println(newDistance);
                 newDistance -= g.getEdge(currentPermutation.get(i), currentPermutation.get(Math.floorMod(i + 1, size)));
-                //System.out.println(newDistance);
                 newPermutation.set(i, currentPermutation.get(j));
                 if (j != Math.floorMod(i + 1, size)) {
                     newDistance -= g.getEdge(currentPermutation.get(Math.floorMod(j - 1, size)), currentPermutation.get(j));
                 }
-               // System.out.println(newDistance);
                 if (i != Math.floorMod(j + 1, size)) {
                     newDistance -= g.getEdge(currentPermutation.get(j), currentPermutation.get(Math.floorMod(j + 1, size)));
                 }
-              //  System.out.println(newDistance);
 
                 newDistance += g.getEdge(newPermutation.get(Math.floorMod(i - 1, size)), newPermutation.get(i));
-               // System.out.println(newDistance);
                 newDistance += g.getEdge(newPermutation.get(i), newPermutation.get(Math.floorMod(i + 1, size)));
-               // System.out.println(newDistance);
                 if (j != Math.floorMod(i + 1, size)) {
                     newDistance += g.getEdge(newPermutation.get(Math.floorMod(j - 1, size)), newPermutation.get(j));
                 }
-               // System.out.println(newDistance);
                 if (i != Math.floorMod(j + 1, size)) {
                     newDistance += g.getEdge(newPermutation.get(j), newPermutation.get(Math.floorMod(j + 1, size)));
                 }
-                //System.out.println(newDistance);
 
-                System.out.println("i " + i + " j " + j);
-                System.out.println("New: " + newDistance);
-                // calculate distance
-                System.out.println("Real " + CostFunction.calcCostFunction(newPermutation, g));
-//                System.out.println(currentPermutation);
-//                System.out.println(newPermutation);
                 if (newDistance < currentDistance) {
                     currentPermutation = new ArrayList<>(newPermutation);
                     currentDistance = newDistance;
